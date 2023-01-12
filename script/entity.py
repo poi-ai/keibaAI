0ea89b07e50aca0b7710a711ced19e0a93e1a628
POI <67753238+poi-ai@users.noreply.github.com>
2023-01-13 01:34:56 +0900

    ミニファイ化したデータクラスファイル作成スクリプト追加

---
+++ script/entity.py
@@ -0,0 +1,5 @@
+exec("import csv\ns='\u0020'\ns4,s8,c_n,p_n,p_d=s*4,s*8,'',[],[]\nfor idx,row in enumerate(csv.reader(open('entity.csv','r'))):\n if idx==0:\n  c_n=row[0]\n  continue\n p_n.append(row[0])\n p_d.append(row[1])")
+i=''.join([f'{s8}self._{p_n[i]}{s}={s}\'{p_d[i]}\'\n' for i in range(len(p_n))])
+g=''.join([f'{s4}@property\n{s4}def{s}{n}(self):\n{s8}return{s}self._{n}\n\n' for n in p_n])
+t=''.join([f'{s4}@{n}.setter\n{s4}def{s}{n}(self,{s}{n}):\n{s8}self._{n}{s}={s}{n}\n\n' for n in p_n])
+open(f'{str.lower(c_n)}.py','w',encoding='utf-8').write(f'class {c_n}:\n{s4}def{s}__init__(self):\n{i}\n{g}\n{t}')
