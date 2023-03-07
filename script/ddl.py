8328a98c4d31f2ee1bbca9bc6cd8a6eadde90e1a
POI <67753238+poi-ai@users.noreply.github.com>
2023-03-08 02:48:32 +0900

    #13 DDL作成処理、8割ほど完成

--- script/ddl.py
+++ script/ddl.py
@@ -1,45 +1,87 @@
+import numpy as np
 import math
 import pandas as pd
+import traceback
 
-# Excelファイルを読み込む
-df = pd.read_excel('create_ddl.xlsx', sheet_name='テーブル定義')
-df.columns = [i for i in range(10)]
+def main():
+    # Excelファイル読み込み
+    try:
+        df = read_excel('create_ddl.xlsx', sheet_name='テーブル定義')
+    except Exception as e:
+        print('テーブル定義書が見つかりません')
 
-# テーブル名取得
-table_name = df[2][0]
+    df.columns = [i for i in range(11)]
 
-if math.isnan(table_name):
-    print('テーブル名が未設定です')
-    exit()
+    # Excelから読み込んだパラメータかたCREATE文を組み立てる
+    try:
+        sql = assemble_sql(df)
+    except Exception as e:
+        print('\n'.join(['CREATE文組み立て処理でエラー', e, traceback.format_exc()]))
 
-ddl = f"CREATE TABLE {table_name} (\n"
+    # TODO .sqlへ出力処理
+    # TODO Excelファイル名変更
+    # TODO templateからファイルコピー
 
-# パラメータの各項目を取得
-for row in df.iterrows():
-    print(df)
-    exit()
-    col_name = row[1][0]
-    data_type = row[1][1]
-    length = row[1][2]
-    pk = row[1][3]
-    fk = row[1][4]
-    not_null = row[1][5]
-    comment = row[1][6]
-    if pd.isna(length):
-        ddl += f"    {col_name} {data_type},\n"
-    else:
-        ddl += f"    {col_name} {data_type}({length}),\n"
-    if pk == 'PK':
-        ddl += f"    PRIMARY KEY ({col_name}),\n"
-    if fk:
-        ddl += f"    FOREIGN KEY ({col_name}) REFERENCES {fk},\n"
-    if not_null == 'NOT NULL':
-        ddl = ddl[:-2] + " NOT NULL,\n"
-    if comment:
-        ddl += f"    COMMENT '{comment}',\n"
-ddl = ddl[:-2] + ");"
+def read_excel():
+    '''Excelファイルを読み込む'''
+    return pd.read_excel('create_ddl.xlsx', sheet_name='テーブル定義')
 
-# COLLATIONは「utf8mb4_general_ci」固定
-ddl += 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci'
+def assemble_sql(df):
+    '''読み込んだパラメータからCREATE文を組み立て'''
 
-print(ddl)
+    # テーブル名取得
+    table_name = df[2][0]
+
+    if nan_check(table_name):
+        print('テーブル名が未設定です')
+        exit()
+
+    ddl = f'CREATE TABLE {table_name} (\n'
+
+    # パラメータの各項目を取得
+    for index, row in enumerate(df.iterrows()):
+        # 1行目はテーブル名、2行目はカラム名なのでパス
+        if index == 0 or index == 1: continue
+
+        # カラム名
+        ddl += f' `{row[1][1]}` '
+
+        # データ型/桁数
+        if 'UNSIGNED' in row[1][2]:
+            ddl += row[1][2].replace(' ', f'({row[1][3]}) ')
+        else:
+            ddl += f'{row[1][2]}'
+            # 桁数
+            if not nan_check(row[1][3]): ddl += f'({row[1][3]})'
+        ddl += ' '
+
+        # NOT NULL(必須)
+        if row[1][4] == '○': ddl += 'NOT NULL '
+
+        # UNIQUE(一意成約)
+        if row[1][5] == '○': ddl += 'UNIQUE '
+
+        # デフォルト値
+        if not nan_check(row[1][8]): ddl += f'DEFAULT {row[1][8]} '
+
+        # コメント
+        if not nan_check(row[1][9]): ddl += f'COMMENT \'{row[1][9]}\''
+
+        ddl += ',\n'
+
+    # TODO 主キー・複合キー・外部キー処理
+    # PRIMARY KEY (order_id, product_id),
+    # FOREIGN KEY (product_id) REFERENCES products (product_id)
+
+    # COLLATIONは「utf8mb4_general_ci」固定
+    ddl += ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;'
+
+    return ddl
+
+def nan_check(text):
+    '''Nanチェック、NanならTrueを返す'''
+    if type(text) != float:
+        return False
+    if math.isnan(text):
+        return True
+    return False
