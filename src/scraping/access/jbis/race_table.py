5c067589691449ba32702d5773b6ebb98313f20e
POI <67753238+poi-ai@users.noreply.github.com>
2023-04-18 01:55:35 +0900

    .
    .

--- src/scraping/access/jbis/race_table.py
+++ src/scraping/access/jbis/race_table.py
@@ -62,13 +62,53 @@ class RaceTable(Base):
         return soup, 1
 
     def get_race_summary(self, soup):
-        '''レース概要を取得'''
+        '''
+        レース概要を取得
+
+        Args:
+            soup(bs4.BeautifulSoup): 出走表ページのHTML
+
+        Reutrns:
+            race_info(dict): レース情報
+                ・race_name()
+                ・horse_class()
+                ・race_type()
+                ・distance()
+                ・age_term()
+                ・country_term()
+                ・local_term()
+                ・load_type()
+                ・gender_term()
+                ・first_prize()
+                ・second_prize()
+                ・third_prize()
+                ・fourth_prize()
+                ・fifth_prize()
+            bool: 処理結果
+
+        '''
+        race_info = {
+            'race_name': '',
+            'horse_class': '',
+            'race_type': '',
+            'distance': '',
+            'age_term': '',
+            'country_term': '',
+            'local_term': '',
+            'load_type': '',
+            'gender_term': '',
+            'first_prize': '',
+            'second_prize': '',
+            'third_prize': '',
+            'fourth_prize': '',
+            'fifth_prize': ''
+        }
 
         # ヘッダのレース番号・レース名を取得
         race_header = soup.find_all('div', class_ = 'box-inner reset-pt-10')
         if len(race_header) == 0:
             self.logger.error('JBIS出走表ページでタイトルの取得に失敗しました')
-            return
+            return None, False
         elif len(race_header) >= 2:
             self.logger.warning('JBIS出走表ページでタイトルクラスが複数見つかりました。最初のタグから抽出を行います。')
 
@@ -77,7 +117,7 @@ class RaceTable(Base):
         if race_name == None:
             self.logger.error('JBIS出走表ページでレース名の取得に失敗しました')
         else:
-            self.race_name,self.horse_class = race_name.groups()
+            race_info['race_name'], race_info['horse_class'] = race_name.groups()
 
         # タイトルの横につく画像からTODOを取得
         str_race_header = str(race_header)
@@ -96,7 +136,7 @@ class RaceTable(Base):
         if course_summary == None:
             self.logger.error('JBIS出走表ページでコース情報の取得に失敗しました')
         else:
-            self.race_type, self.distance = course_summary.groups()
+            race_info['race_type'], race_info['distance'] = course_summary.groups()
 
         # レース条件
         race_condition_summary = str(race_summary[0].find_all('li', class_ = 'first-child')[0])
@@ -106,139 +146,204 @@ class RaceTable(Base):
 
         if age_term_match == None:
             if 'サラ系一般' in race_condition_summary:
-                self.age_term = '一般'
+                race_info['age_term'] = '一般'
             else:
                 self.logger.error('JBIS出走表ページで馬齢条件の取得に失敗しました')
         else:
             if age_term_match.groups()[1] == '上':
-                self.age_term = mold.full_to_half(age_term_match.groups()[0]) + '歳上'
+                race_info['age_term'] = mold.full_to_half(age_term_match.groups()[0]) + '歳上'
             else:
-                self.age_term = mold.full_to_half(age_term_match.groups()[0]) + '歳'
+                race_info['age_term'] = mold.full_to_half(age_term_match.groups()[0]) + '歳'
 
         # 国籍条件
         if '（国際）' in race_condition_summary:
-            self.country_term = '国際'
+            race_info['country_term'] = '国際'
         elif '（混合）' in race_condition_summary:
-            self.country_term = '混合'
+            race_info['country_term'] = '混合'
 
         # 地方条件
         if '（特指）' in race_condition_summary:
-            self.local_term = '特指'
+            race_info['local_term'] = '特指'
         elif '（指定）' in race_condition_summary: # TODO カク指、マル指
-            self.local_term = '指定'
+            race_info['local_term'] = '指定'
 
         # 斤量条件
         if '定量' in race_condition_summary:
-            self.load_type = '定量'
+            race_info['load_type'] = '定量'
         elif '馬齢' in race_condition_summary:
-            self.load_type = '馬齢'
+            race_info['load_type'] = '馬齢'
         elif '別定' in race_condition_summary:
-            self.load_type = '別定'
+            race_info['load_type'] = '別定'
         elif 'ハンデ' in race_condition_summary:
-            self.load_type = 'ハンデ'
+            race_info['load_type'] = 'ハンデ'
 
         # 性別条件
         if '牡・牝' in race_condition_summary:
-            self.gender_term = '牡牝'
+            race_info['gender_term'] = '牡牝'
         elif '牝' in race_condition_summary:
-            self.gender_term = '牝'
+            race_info['gender_term'] = '牝'
 
         # クラス名のないliタグ内から情報取得
         for li in race_summary[0].find_all('li'):
             # 1着賞金
             first_prize_match = re.search('1着：(.+)円', str(li))
             if first_prize_match != None:
-                self.first_prize = first_prize_match.groups()[0].replace(',', '')
+                race_info['first_prize'] = first_prize_match.groups()[0].replace(',', '')
 
             # 2着賞金
             second_prize_match = re.search('2着：(.+)円', str(li))
             if second_prize_match != None:
-                self.second_prize = second_prize_match.groups()[0].replace(',', '')
+                race_info['second_prize'] = second_prize_match.groups()[0].replace(',', '')
 
             # 3着賞金
             third_prize_match = re.search('3着：(.+)円', str(li))
             if third_prize_match != None:
-                self.third_prize = third_prize_match.groups()[0].replace(',', '')
+                race_info['third_prize'] = third_prize_match.groups()[0].replace(',', '')
 
             # 4着賞金
             fourth_prize_match = re.search('4着：(.+)円', str(li))
             if fourth_prize_match != None:
-                self.fourth_prize = fourth_prize_match.groups()[0].replace(',', '')
+                race_info['fourth_prize'] = fourth_prize_match.groups()[0].replace(',', '')
 
             # 5着賞金
             fifth_prize_match = re.search('5着：(.+)円', str(li))
             if fifth_prize_match != None:
-                self.fifth_prize = fifth_prize_match.groups()[0].replace(',', '')
+                race_info['fifth_prize'] = fifth_prize_match.groups()[0].replace(',', '')
 
-        return
+        return race_info, False
 
     def get_horse_data(self, soup):
-        '''出馬表からデータ抽出'''
+        '''
+        出馬表からデータ抽出
+
+        Args:
+            soup(bs4.BeautifulSoup): 出走表ページのHTML
+
+        Returns:
+            horse_info_list[dict, dict]: 出走馬情報のリスト
+                ・horse_no()
+                ・frame_no()
+                ・gender()
+                ・age()
+                ・hair_color()
+                ・horse_id()
+                ・horse_name()
+                ・trainer_id()
+                ・trainer_name()
+                ・trainer_belong()
+                ・jockey_id()
+                ・jockey()
+                ・load_name()
+                ・owner_id()
+                ・owner_name()
+                ・breeder_id()
+                ・breeder_name()
+                ・father_id()
+                ・father_name()
+                ・mother_id()
+                ・mother()
+                ・mf_id()
+                ・mf_name()
+            bool: 処理結果
+
+        '''
         tables = soup.find_all('table', class_ = 'tbl-data-04 tbl-nomination')
         if len(tables) == 0:
             self.logger.error('JBIS出走表ページで出走表テーブルの取得に失敗しました')
-            return
+            return None, False
         elif len(tables) >= 2:
             self.logger.warning('JBIS出走表ページで出走表テーブルが複数見つかりました。最初に出現したテーブルから抽出を行います。')
+
+        horse_info_list = []
 
         # 出馬表テーブルから一行ずつ取得
         for index, tr in enumerate(tables[0].find_all('tr')):
             # 一行目はカラム名なのでスキップ
             if index == 0: continue
 
+            horse_info = {
+                'horse_no': '',
+                'frame_no': '',
+                'gender': '',
+                'age': '',
+                'hair_color': '',
+                'horse_id': '',
+                'horse_name': '',
+                'trainer_id': '',
+                'trainer_name': '',
+                'trainer_belong': '',
+                'jockey_id': '',
+                'jockey': '',
+                'load_name': '',
+                'owner_id': '',
+                'owner_name': '',
+                'breeder_id': '',
+                'breeder_name': '',
+                'father_id': '',
+                'father_name': '',
+                'mother_id': '',
+                'mother': '',
+                'mf_id': '',
+                'mf_name': ''
+            }
+
             # 登録頭数
             self.registed_num = len(tables[0].find_all('tr'))
 
             # 馬番・枠番
-            self.horse_no, self.frame_no = index, culc.frame_no_culc(self.registed_num, index)
+            horse_info['horse_no'], horse_info['frame_no'] = index, culc.frame_no_culc(self.registed_num, index)
 
             # TODO ここらへんのmatch処理を関数化すべきか 読みやすさはしない方が上だと思う
             # tdタグを全て取得して一つずつ中身をチェック
             for td in tr.find_all('td'):
                 summary_match = re.search('([牡|牝|セン])([1-2]?[0-9])<br/>(.{1,2})</td>', str(td))
                 if summary_match != None:
-                    self.gender, self.age, self.hair_color = summary_match.groups()
+                    horse_info['gender'], horse_info['age'], horse_info['hair_color'] = summary_match.groups()
 
                 # 馬名
                 horse_match = re.search('<a href="/horse/(\d+)/">(.+)</a>', str(td))
                 if horse_match != None:
-                    self.horse_id, self.horse_name = horse_match.groups()
+                    horse_info['horse_id'], horse_info['horse_name'] = horse_match.groups()
 
                 # 調教師
                 trainer_match = re.search('<a href="/race/trainer/(\d+)/">(.+)</a>（(.+)）', str(td))
                 if trainer_match != None:
-                    self.trainer_id, self.trainer, self.trainer_belong = trainer_match.groups()
+                    horse_info['trainer_id'], horse_info['trainer_name'], horse_info['trainer_belong'] = trainer_match.groups()
 
                 # 騎手・斤量・減量騎手
                 jockey_match = re.search('<a href="/race/jockey/(\d+)/">(.+)</a>', str(td))
                 if jockey_match != None:
-                    self.jockey_id, self.jockey = jockey_match.groups()
-                    self.load = td.find('span').text
+                    horse_info['jockey_id'], horse_info['jockey_name'] = jockey_match.groups()
+                    horse_info['load'] = td.find('span').text
                     # TODO 減量チェック処理
 
                 # 馬主
                 owner_match = re.search('<a href="/race/owner/(\d+)/">(.+)</a>', str(td))
                 if owner_match != None:
-                    self.owner_id, self.owner = owner_match.groups()
+                    horse_info['owner_id'], horse_info['owner_name'] = owner_match.groups()
 
                 # 生産牧場
                 breeder_match = re.search('<a href="/breeder/(\d+)/">(.+)</a>', str(td))
                 if breeder_match != None:
-                    self.breeder_id, self.breeder = breeder_match.groups()
+                    horse_info['breeder_id'], horse_info['breeder_name'] = breeder_match.groups()
 
             # liタグを全て取得して一つずつ中身をチェック
             for li in tr.find_all('li'):
                 # 父名
                 father_match = re.search('父：<a href="/horse/(\d+)/"(.+)</a>', str(li))
                 if father_match != None:
-                    self.father_id, self.father = father_match.groups()
+                    horse_info['father_id'], horse_info['father_name'] = father_match.groups()
 
                 # 母名
                 mother_match = re.search('母：<a href="/horse/(\d+)/"(.+)</a>', str(li))
                 if mother_match != None:
-                    self.mother_id, self.mother = mother_match.groups()
+                    horse_info['mother_id'], horse_info['mother_name'] = mother_match.groups()
 
                 # 母父名
                 groundfather_match = re.search('母父：<a href="/horse/(\d+)/"(.+)</a>', str(li))
                 if groundfather_match != None:
-                    self.groundfather_id, self.groundfather = groundfather_match.groups()
+                    horse_info['mf_id'], horse_info['mf_name'] = groundfather_match.groups()
+
+            horse_info_list.append(horse_info)
+
+        return horse_info_list, True
