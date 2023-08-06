90c2fa03923f7e7cb9e4e32b28dff8a07041df1d
POI <67753238+poi-ai@users.noreply.github.com>
2023-08-07 02:32:15 +0900

    枠だけ作成

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+import analysis
+import common
+import scraping
+
+class KeibaAi():
+    def __init__(self):
+        '''
+        初期処理
+
+        '''
+        pass
---
+++ src/scraping/__init__.py
@@ -0,0 +1,9 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+import access
+import db
+import entity
+
+class Scraping():
+    pass
