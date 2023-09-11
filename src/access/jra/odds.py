a74240ddab7fe076a6d10aed8838c1c64e07b431
POI <POI>
2023-09-11 18:41:22 +0900

    微修正

--- src/access/jra/odds.py
+++ src/access/jra/odds.py
@@ -102,14 +102,31 @@ class Odds(Base, DoAction):
                 umatan: 馬単、trio: 三連複、tirece: 三連単、unknown: 不明
 
         '''
+        # HTML取得
         soup = self.do_action(self.ODDS_BASE_URL, cname)
+
+        # ボタンをアクティブにする(=へこませる)ためのCSSを呼ぶクラスを持つ項目を取得
         current_list = soup.find_all('li', class_ = 'current')
+
+        # 一つずつ見ていき当てはまるものを返す
         for current in current_list:
             c = current.text
             if '単勝・複勝' in c:
                 return 'tanpuku'
-            elif 'a' in c:
-                return 'unknown'
+            elif '枠連' in c:
+                return 'wakuren'
+            elif '馬連' in c:
+                return 'wide'
+            elif '馬単' in c:
+                return 'umatan'
+            elif 'ワイド' in c:
+                return 'wide'
+            elif '3連複' in c:
+                return 'trio'
+            elif '3連単' in c:
+                return 'tirece'
+        # どれも一致しなかった場合
+        return 'unknown'
 
 o = Odds()
 print(o.judge_page_type('pw151ou1004202303080120230903Z/F0'))
